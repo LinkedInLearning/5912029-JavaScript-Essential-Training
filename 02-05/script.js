87c0b9532f791945b44e8d590199c5e058eb910c
OKI Yoshiya <oki.yoshiya@ceroan.co.jp>
2024-04-08 09:19:51 +0000

    プログラムを追加した

---
+++ 02-05/script.js
@@ -0,0 +1,12 @@
+const myNumber = -37;
+
+console.log(3 + 2);
+console.log(8 - 6);
+console.log(4 * 3);
+console.log(8 / 2);
+
+console.log(4 * (2 + 6));
+
+let plusNumber = 5;
+plusNumber += 1;
+console.log(plusNumber);
