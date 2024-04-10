a76746587574e1151d94ce2b496474e9c8945390
OKI Yoshiya <oki.yoshiya@ceroan.co.jp>
2024-04-10 02:48:48 +0000

    プログラムを追加した

---
+++ 05-02/script.js
@@ -0,0 +1,5 @@
+const myDate = new Date();
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth()); // 0～11（0は1月）
+console.log(myDate.getDate());
+console.log(myDate.getDay()); // 0～6（0は日曜日）
