3cbcc8c9290ed52fd0dbc0aad8f66a98fa3810f0
OKI Yoshiya <oki.yoshiya@ceroan.co.jp>
2024-04-10 07:30:56 +0000

    プログラムを追加した

---
+++ 05-06/script.js
@@ -0,0 +1,14 @@
+const user = {
+    name: "沖 良矢",
+    age: 37,
+    hasChild: true,
+};
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+user.age = 42;
+console.log(user.age);
+console.log(user.address);
+user.address = "東京都";
+console.log(user.address);
+console.log(user);
