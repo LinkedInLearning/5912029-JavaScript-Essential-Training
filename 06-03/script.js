faeb6944094ec602e6633901c5b5e132a8299b73
OKI Yoshiya <oki.yoshiya@ceroan.co.jp>
2024-04-10 10:27:28 +0000

    プログラムを追加した

---
+++ 06-03/script.js
@@ -0,0 +1,6 @@
+const myArray = ["しりとり", "りんご", "ゴリラ", "ラッパ", "パセリ"];
+console.log(myArray.length);
+myArray.splice(2, 2);
+console.log(myArray);
+myArray.splice(1, 0, "りょうり");
+console.log(myArray);
